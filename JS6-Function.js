1a21314348d7634a6c8c3068faea42dc4948738c
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-24 11:00:49 +0700

    update js

---
+++ JS6-Function.js
@@ -0,0 +1,21 @@
+function hi() {
+    console.log("Hi Tento");
+}
+
+function sum(a,b) {
+    return a+b    
+}
+function cekAngka(n) {
+    if (n%2==0) {
+        console.log("Even");
+    }
+    else{
+        console.log("Odd");
+        
+    }
+}
+
+let sumFum=sum(2,3);
+hi();
+console.log(sumFum);
+cekAngka(2);
---
+++ JS7-Object.js
@@ -0,0 +1,6 @@
+const person ={
+    firstName:"Tento",
+    age:21,
+    lang:"js"
+}
+console.log(person.firstName);
