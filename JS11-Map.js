38351a88a6b10c4045d4d78775ab0fa6ab5aa6dc
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-24 11:20:08 +0700

    map

---
+++ JS11-Map.js
@@ -0,0 +1,10 @@
+const riders=new Map();
+
+riders.set("Tendou","Kabuto")
+riders.set("Takumi","Faiz")
+riders.set("Sentou","Build")
+riders.set("t","Kiva")
+
+for(const rider of riders){
+    console.log(rider);
+}
